b466a86fafe2608b8b928f81f7799affd08a6b92
Wes Pearce <wespearce1@gmail.com>
2018-07-31 16:09:42 -0500

    Created Bill component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Bill from './Bill'
+import '../style.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <Bill />
       </div>
     );
   }
