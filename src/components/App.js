eb9d68ac414110571ac0f8dc21bb9fde5f7cb789
Wes Pearce <wespearce1@gmail.com>
2018-08-12 17:16:53 -0500

    Cleaned up code and added comments.

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,8 @@ import BillViewer from '../containers/BillViewer'
 import BillEditor from '../containers/BillEditor'
 import '../style.css'
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'baseline'
-  }
-})
-
 class App extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
-    const classes = this.props
-
     return (
       <div className="my-container">
         <BillEditor />
