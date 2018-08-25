4fc51ad66cded50ef15747490f8dd90d5688c056
Wes Pearce <wespearce1@gmail.com>
2018-08-25 18:00:59 -0500

    Cleaning up for a 1st deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import billit from './reducers/reducers'
 import store from './store'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/jquery/dist/jquery.min.js'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
