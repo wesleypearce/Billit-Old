9ceb0731097557a7f8a20e51a7b89a4a8ae10a54
Wes Pearce <wespearce1@gmail.com>
2018-08-29 20:11:34 -0500

    Responsive design on App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,72 @@
 import React, { Component } from 'react';
 import BillViewer from './containers/BillViewer'
 import BillEditor from './containers/BillEditor'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 import './style.css'
 
-const jumbotronStyle = {
-  fontFamily: 'Roboto',
-  fontSize: '16px',
-  backgroundColor: 'rgb(61, 3, 108)',
-  color: 'white',
-  textAlign: 'center'
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#6a1b9a',
+    },
+    secondary: {
+      light: '#b6ffff',
+      main: '#81d4fa',
+      contrastText: '#000000',
+    },
+  },
+})
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly'
+  },
+  billEditor: {
+    flexBasis: '40%'
+  },
+  billViewer: {
+    flexBasis: '60%'
+  },
+  appBar: {
+    marginBottom: '30px'
+  }
 }
 
 class App extends Component {
+
   render() {
+    const { classes } = this.props
     return (
-      <div>
-        <div style={jumbotronStyle} className="jumbotron">
-          <h1>Billit!</h1>
-          <p>
-            A quick, effective way to track your upcoming bills
-          </p>
-        </div>
-        <div className="my-container">
-          <BillEditor />
-          <BillViewer />
-        </div>
+      <MuiThemeProvider theme={theme}>
+      <div className={classes.container}>
+        <AppBar className={classes.appBar} position="static" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Billit!
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <BillEditor className={classes.billEditor} />
+        <BillViewer className={classes.billViewer} />
       </div>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App);
