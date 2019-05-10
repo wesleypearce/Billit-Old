e2900b909d5c3c47f99abab75f7a0c78dd13254c
Wes Pearce <wespearce1@gmail.com>
2019-05-10 17:23:35 -0600

    Created basic html components for CreateBill form

--- client/src/containers/CreateBill.js
+++ client/src/containers/CreateBill.js
@@ -10,6 +10,18 @@ class CreateBill extends React.Component {
             <input className="input" type="text" placeholder="Name" />          
           </div>
         </div>
+        <div className="field">
+          <label className="label">Cost</label>
+          <div className="control">
+            <input className="input" type="number" placeholder="0" />          
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Due Date</label>
+          <div className="control">
+            <input className="input" type="date" placeholder={Date.now()} />          
+          </div>
+        </div>
       </div>
     )
   }
