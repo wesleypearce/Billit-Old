36bae9effea537643464a94a88c8aa3d0b0554a8
Wes Pearce <wespearce1@gmail.com>
2019-05-11 08:43:53 -0600

    NavBar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import CreateBill from './components/CreateBill'
 import BillList from './components/BillList'
+import NavBar from './components/NavBar'
 import '../node_modules/bulma/css/bulma.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="columns">
-        <div className="column">
-          <CreateBill />
-        </div>
-        <div className="column">
-          <BillList />
+      <div>
+        <NavBar />
+        <div className="columns">
+          <div className="column">
+            <CreateBill />
+          </div>
+          <div className="column">
+            <BillList />
+          </div>
         </div>
       </div>
     )
