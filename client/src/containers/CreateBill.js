fdd49987a41e3b5f5d8bf49a08047402e419cfb7
Wes Pearce <wespearce1@gmail.com>
2019-05-11 07:31:24 -0600

    CreateBill component created

--- client/src/containers/CreateBill.js
+++ client/src/containers/CreateBill.js
@@ -1,30 +1,60 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createBill } from '../actions'
 
 class CreateBill extends React.Component {
+  state = {
+    name: '',
+    cost: 0,
+    dueDate: ''
+  }
+
+  handleClick = () => {
+    this.props.CreateBill(this.state)
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="field">
           <label className="label">Name</label>
           <div className="control">
-            <input className="input" type="text" placeholder="Name" />          
+            <input className="input" onChange={this.handleInputChange} type="text" placeholder="" />          
           </div>
         </div>
         <div className="field">
           <label className="label">Cost</label>
           <div className="control">
-            <input className="input" type="number" placeholder="0" />          
+            <input className="input" onChange={this.handleInputChange} type="number" placeholder="" />          
           </div>
         </div>
         <div className="field">
           <label className="label">Due Date</label>
           <div className="control">
-            <input className="input" type="date" placeholder={Date.now()} />          
+            <input className="input" onChange={this.handleInputChange} type="date" placeholder={Date.now()} />          
           </div>
+        </div>
+        <div className="control">
+          <button className="button is-primary" onClick={this.handleClick}>Create</button>
         </div>
       </div>
     )
   }
 }
 
-export default CreateBill
+const mapStateToProps = state => {
+  return {
+    bills: state.bills
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { createBill }
+)(CreateBill)
