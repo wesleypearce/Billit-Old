a6c22c2335fd5f23c38dbbadaad73b80723aedda
Wes Pearce <wespearce1@gmail.com>
2019-06-09 07:14:11 -0600

    EditBill component designed

--- client/src/components/bills/EditBill.js
+++ client/src/components/bills/EditBill.js
@@ -1,35 +1,97 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { createBill } from '../../actions'
 
 class EditBill extends React.Component {
-  render() {
-    console.log(this.props.match.params.id)
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <h3 className="">{error}</h3>
+      )
+    }
+  }
+  renderInput = ({ input, label, type, meta }) => {
+    console.log(meta)
     return (
       <div>
-        <h1 className="title">Edit a Bill</h1>
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input className="input" onChange={this.handleInputChange} name="name" type="text" placeholder="" />          
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Cost</label>
-          <div className="control">
-            <input className="input" onChange={this.handleInputChange} name="cost" type="number" placeholder="" />          
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Due Date</label>
-          <div className="control">
-            <input className="input" onChange={this.handleInputChange} name="dueDate" type="date" placeholder={Date.now()} />          
-          </div>
-        </div>
+        <label className="label">{label}</label>
         <div className="control">
-          <button className="button is-primary" onClick={this.handleClick}>Create</button>
+          <input className="input" 
+            type={type} 
+            {...input} />
+          {this.renderError(meta)}
         </div>
+      </div>
+    )
+  }
+
+  onSubmit = (formValues) => {
+    this.props.createBill(formValues)
+  }
+
+  render() {
+    return (
+      <div className="block">
+      <h1 className="title">Edit a Bill</h1>
+      <h1 className="subtitle">{this.props.bill.name}</h1>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div className="field">
+            <Field name="name" component={this.renderInput} label="Bill name" type="text" />
+            <Field name="cost" component={this.renderInput} label="Bill cost" type="number" />
+            <Field name="dueDate" component={this.renderInput} label="Bill due date" type="date" />
+            <div className="control">
+              <button className="button is-primary">Submit</button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
 }
 
-export default EditBill
+const validate = formValues => {
+  const errors = {}
+
+  if (!formValues.name) {
+    errors.name = 'You must enter a name'
+  }
+
+  if (!formValues.cost) {
+    errors.cost = 'You must enter a cost'
+  }
+
+  if (!formValues.dueDate) {
+    errors.dueDate = 'You must enter a due date'
+  }
+
+  return errors
+}
+
+const mapStateToProps = state => {
+  return {
+    bill: state.bills.selectedBill
+  }
+}
+
+
+
+// const formWrapped = reduxForm({
+//   form: 'createBill',
+//   validate
+// })(CreateBill)
+
+// export default connect(
+//   mapStateToProps,
+//   { createBill }
+// )(formWrapped)
+
+const wrapped = connect(
+  mapStateToProps,
+  { createBill }
+)(EditBill)
+
+export default reduxForm({
+  form: 'createBill',
+  validate
+})(wrapped)
