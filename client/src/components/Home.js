854a13a9bfc5f91bd09198947b4861c34a0ae6d2
Wes Pearce <wespearce1@gmail.com>
2019-06-09 19:57:59 -0600

    Changed title page paragraph

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <React.Fragment>
       <div className="content is-large">
         <h1>Billit!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>Welcome to Billit! Billit helps you keep track of your upcoming bills. Don't be late on a single payment!</p>
       </div>
       <div className="has-text-centered">
         <Link className="button is-success" to="bills/new">Create a Bill</Link>
